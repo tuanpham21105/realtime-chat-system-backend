7185c30a29e402a61414d5934b8a3d8472d685ee
tuanpham21105 <tuan.phamminh9936@gmail.com>
2026-01-26 10:19:32 +0700

    add: Api gateway;

---
+++ api_gateway/src/test/java/com/chat_system/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat_system.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
